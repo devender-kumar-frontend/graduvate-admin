e1c9024bce1a81be691d3140147741bcd6667aad
rakeshcyberworx <rakesh.cyberworx@gmail.com>
2025-03-16 21:19:54 +0530

    create testunibuak and event collection

---
+++ src/collections/Events/index.ts
@@ -0,0 +1,33 @@
+import { CollectionConfig } from 'payload'
+
+export const Events: CollectionConfig = {
+  slug: 'events', // Unique identifier for the collection
+  labels: {
+    singular: 'Event',
+    plural: 'Events',
+  },
+  admin: {
+    useAsTitle: 'name',
+  },
+  fields: [
+    {
+      name: 'name',
+      type: 'text',
+      required: true,
+    },
+    {
+      name: 'designation',
+      type: 'text',
+      required: true,
+    },
+    {
+      name: 'description',
+      type: 'textarea',
+    },
+    {
+      name: 'image',
+      type: 'upload',
+      relationTo: 'media',
+    },
+  ],
+}
