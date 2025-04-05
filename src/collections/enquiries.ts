a550d8544b35fdb7dfa86eb74d2cb5815b9eb53c
Yasin <yasin@cyberworx.in>
2025-04-05 22:57:21 +0530

    finance form done

---
+++ src/collections/enquiries.ts
@@ -0,0 +1,70 @@
+import { CollectionConfig } from 'payload';
+
+export const Enquiries: CollectionConfig = {
+  slug: 'enquiries',
+  admin: {
+    useAsTitle: 'name',
+  },
+  access: {
+    create: () => true,
+    read: () => true,
+    update: () => true,
+    delete: () => true,
+  },
+  fields: [
+    {
+      name: 'name',
+      label:'Full Name',
+      type: 'text',
+    },
+    {
+      name: 'email',
+      label: 'Email',
+      type: 'email',
+    },
+    {
+      name: 'number',
+      label: 'Phone Number',
+      type: 'text',
+    },
+    {
+      name:'amount',
+      label: 'Loan Amount',
+      type: 'text',
+    },
+    {
+      name:'duration',
+      label:'Course Duration',
+      type:'text',
+    }
+  ],
+  endpoints: [
+    {
+      path: '/finanace-enquiry',
+      method: 'post',
+      handler: async (req) => {
+        try {
+          const data = await req.json();
+          const newSubmission = await req.payload.create({
+            collection: 'enquiries',
+            data: {
+              name:data.name,
+              email:data.email,
+              number:data.number,
+              amount:data.amount,
+              duration:data.duration
+            },
+          });
+
+          return Response.json({
+            message: 'Submission saved successfully',
+            enquiry: newSubmission,
+          },{status:201});
+
+        } catch (error: any) {
+          return Response.json({ message: 'Internal server error', error: error.message });
+        }
+      },
+    },
+  ],
+}
